9600051c1c3e58a157793bed734f5ff269bf6b53
Raheem <raheempayne4@gmail.com>
2020-02-17 17:19:44 -0500

    Week04 Homework

---
+++ Homework/Week04/RaheemPayne/Problem1/myIfScript.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+if [ x1 \< x2 ]
+then 
+	echo "x1 is less than x2"
+
+fi
+
+
+if [[newNumber == oldNumber]]
+then
+	echo "Numbers are the same"
+else
+	echo "Numbers are not the same"
+fi
+
