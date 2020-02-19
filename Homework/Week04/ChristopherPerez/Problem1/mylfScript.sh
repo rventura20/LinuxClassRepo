86e5c736da920e13d8a1c93921bd93d97bafeeeb
Chris <alf.cpu@gmail.com>
2020-02-19 02:20:00 +0000

    got the second if to work.

--- Homework/Week04/ChristopherPerez/Problem1/mylfScript.sh
+++ Homework/Week04/ChristopherPerez/Problem1/mylfScript.sh
@@ -10,3 +10,14 @@ then
 else
 	echo not nice number
 fi
+
+read -sp "Whisper this number in me in private, no one will be able to see it! " num2
+echo ""
+if [[ "$num" < "$num2" ]]
+then
+	echo "Your original number, $num, is less than your second number."
+else
+	echo "Your original number, $num, is greater than or equal to your second number."
+
+fi
+
