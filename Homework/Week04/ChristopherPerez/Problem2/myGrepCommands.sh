1d1d56d2b12f25e1f905977fde01c36c9affcd90
Chris <alf.cpu@gmail.com>
2020-02-19 02:49:05 +0000

    it's  all falling apart

--- Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
+++ Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-grep "^Did" myGrepFile.txt
+grep "^D" myGrepFile.txt
+echo "^D"
 grep "c$" myGrepFile.txt
-grep "^[a-z]" myGrepFile.txt
+echo "c$"
+grep "[0-9]+" myGrepFile.txt
+echo "[0-9]+"
+grep "[a-z]" myGrepFile.txt
+echo "[a-z]"
+grep "[A-Z]" myGrepFile.txt
+echo "[A-Z]"
+grep "{5,}" myGrepFile.txt
+echo "[A-Z]"
