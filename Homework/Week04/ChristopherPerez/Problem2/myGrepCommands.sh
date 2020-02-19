569b30632ed79d9f68f621d0337f4ebcd869c22c
Chris <alf.cpu@gmail.com>
2020-02-19 03:26:52 +0000

    winner time for prb 2

--- Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
+++ Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-echo "1. Ends in f"
 grep "f$" myGrepFile.txt
-echo "2. Starts with lower case letter"
 grep "^[a-z]" myGrepFile.txt
-echo "3. Starts with number"
 grep "^[0-9]" myGrepFile.txt
-echo "4. I 5 times"
 grep "I\{5,\}" myGrepFile.txt
-echo "5. Starts with J"
 grep -i "^j" myGrepFile.txt
