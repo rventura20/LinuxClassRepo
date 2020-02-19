dcb23eb1a1f72c5f42b5a304dec60bf83c0e064b
Chris <alf.cpu@gmail.com>
2020-02-19 03:25:57 +0000

    Verbose code complete

--- Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
+++ Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-echo "1. c$"
-grep "c$" myGrepFile.txt
-echo "2. ^[a-z]"
+echo "1. Ends in f"
+grep "f$" myGrepFile.txt
+echo "2. Starts with lower case letter"
 grep "^[a-z]" myGrepFile.txt
-echo "3. I{5}"
-grep "I{5}" myGrepFile.txt
+echo "3. Starts with number"
+grep "^[0-9]" myGrepFile.txt
+echo "4. I 5 times"
+grep "I\{5,\}" myGrepFile.txt
+echo "5. Starts with J"
+grep -i "^j" myGrepFile.txt
