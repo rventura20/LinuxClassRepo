250b2dfee5add40d648d11a3d1fda0174f66a65b
Chris <alf.cpu@gmail.com>
2020-02-19 02:58:11 +0000

    sanity is reaching it's maximum deceleration rate

--- Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
+++ Homework/Week04/ChristopherPerez/Problem2/myGrepCommands.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
-grep "^D" myGrepFile.txt
-echo "^D"
+echo "1. c$"
 grep "c$" myGrepFile.txt
-echo "c$"
-grep "[0-9]+" myGrepFile.txt
-echo "[0-9]+"
-grep "[a-z]" myGrepFile.txt
-echo "[a-z]"
-grep "[A-Z]" myGrepFile.txt
-echo "[A-Z]"
-grep "{5,}" myGrepFile.txt
-echo "[A-Z]"
+echo "2. ^[a-z]"
+grep "^[a-z]" myGrepFile.txt
+echo "3. I{5}"
+grep "I{5}" myGrepFile.txt
