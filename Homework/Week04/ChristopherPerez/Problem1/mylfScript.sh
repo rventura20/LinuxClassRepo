7bd0263ba89486c87a8dc4ce0371a8d896bc0526
Chris <alf.cpu@gmail.com>
2020-02-19 02:11:40 +0000

    first if statement works fine

--- Homework/Week04/ChristopherPerez/Problem1/mylfScript.sh
+++ Homework/Week04/ChristopherPerez/Problem1/mylfScript.sh
@@ -4,4 +4,9 @@
 echo The first if will check if your number is greater than 20.
 read -p "Choose a number, any number: " num
 
-echo I like the number $num
+if [ $num -gt 20 ]
+then
+	echo nice number
+else
+	echo not nice number
+fi
