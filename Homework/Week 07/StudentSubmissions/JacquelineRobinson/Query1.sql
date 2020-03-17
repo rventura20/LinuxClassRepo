0e54e78bd9b2a71bf99de0ad7cabb4e90dfb7df7
ja-eng <57498315+ja-eng@users.noreply.github.com>
2020-03-17 01:41:49 -0400

    Create Query1.sql

---
+++ Homework/Week 07/StudentSubmissions/JacquelineRobinson/Query1.sql
@@ -0,0 +1,2 @@
+SELECT * FROM crime_scene_report WHERE type = "murder" and date = 20180115 and city = "SQL City";
+20180115|murder|Security footage shows that there were 2 witnesses. The first witness lives at the last house on "Northwestern Dr". The second witness, named Annabel, lives somewhere on "Franklin Ave".|SQL City
