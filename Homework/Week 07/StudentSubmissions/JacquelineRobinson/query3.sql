2e7def86b4f133cf2893ae03aadd76ad3377a8b6
ja-eng <57498315+ja-eng@users.noreply.github.com>
2020-03-17 02:48:39 -0400

    Update query3.sql

--- Homework/Week 07/StudentSubmissions/JacquelineRobinson/query3.sql
+++ Homework/Week 07/StudentSubmissions/JacquelineRobinson/query3.sql
@@ -1,1 +1,1 @@
-SELECT * FROM person WHERE address_number = 4919 and address_street_name = "Northwestern Dr";
+SELECT * FROM person WHERE name LIKE "Annabel" AND address_street_name = "Franklin Ave";
