9ad5ae8002d7d204173adddf38a4b1ae61ad4283
ja-eng <57498315+ja-eng@users.noreply.github.com>
2020-03-17 21:22:38 -0400

    Update query2.sql

--- Homework/Week 07/StudentSubmissions/JacquelineRobinson/query2.sql
+++ Homework/Week 07/StudentSubmissions/JacquelineRobinson/query2.sql
@@ -1,1 +1,1 @@
-Select * FROM person WHERE address_street_name = "Northwestern Dr" ORDER BY address_number DESC;
+SELECT * FROM person WHERE address_street_name = "Northwestern Dr" ORDER BY address_number DESC;
