79f04b89294619d6b46d9faf93dc8cdbd93795ff
ja-eng <57498315+ja-eng@users.noreply.github.com>
2020-03-17 21:54:22 -0400

    Update query2.sql

--- Homework/Week 07/StudentSubmissions/JacquelineRobinson/query2.sql
+++ Homework/Week 07/StudentSubmissions/JacquelineRobinson/query2.sql
@@ -1,1 +1,1 @@
-SELECT * FROM person WHERE address_street_name = "Northwestern Dr" ORDER BY address_number DESC;
+SELECT * FROM person WHERE address_street_name = "Northwestern Dr" ORDER BY address_number DESC LIMIT (SELECT COUNT(*) FROM person GROUP BY address_number ORDER BY address_number DESC LIMIT 1);
