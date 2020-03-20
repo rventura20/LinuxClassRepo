1a5bfb15afe73687ab47e2a1703a177e5d505807
FabricioPaguay <fabriciopaguay098@gmail.com>
2020-03-20 17:51:12 -0400

    examQuestions.sh

--- Exams/Midterm/FabricioPaguay/examQuestions.sh
+++ Exams/Midterm/FabricioPaguay/examQuestions.sh
@@ -1,16 +1,69 @@
 question1(){
-	echo "todo"
+	echo "What it means SQL?" 
+	echo "a) Structure Query Languaje" 
+	echo "b) Secuence Query Languaje" 
+	echo "c) Software Query Languaje" 
+	read answer 
+	case $answer in
+		"a")
+			echo "fabricio question 1 right" >> $ANSWER_FILE
+			;;
+		"b")
+			echo "fabricio question 1 wrong" >> $ANSWER_FILE
+			;;
+		"c")
+			echo "fabricio question 1 wrong" >> $ANSWER_FILE
+			;;
+		*)
+			echo "Please enter a valid selection from a-c" 
+			question1
+			;;
+	esac
 }
-
 question2(){
-	echo "todo"
+	echo "what .db stands for?" 
+	echo "a) databoard" 
+	echo "b) database" 
+	echo "c) docbase" 
+	read answer 
+	case $answer in
+		"a")
+			echo "fabricio question 2 wrong" >> $ANSWER_FILE
+			;;
+		"b")
+			echo "fabricio question 2 right" >> $ANSWER_FILE
+			;;
+		"c")
+			echo "fabricio question 2 wrong" >> $ANSWER_FILE
+			;;
+		*)
+			echo "Please enter a valid selection from a-c" 
+			question2
+			;;
+	esac
 }
-
 question3(){
-	echo "todo"
+	echo "A11 SQL statements ends with?" 
+	echo "a) /"
+	echo "b) }" 
+	echo "c) ;" 
+	read answer 
+	case $answer in
+		"a")
+			echo "fabricio question 3 wrong" >> $ANSWER_FILE
+			;;
+		"b")
+			echo "fabricio question 3 wrong" >> $ANSWER_FILE
+			;;
+		"c")
+			echo "fabricio question 3 right" >> $ANSWER_FILE
+			;;
+		*)
+			echo "Please enter a valid selection from a-c"
+			question3
+			;;
+	esac
 }
-
-
 
 run_fabriciopaguays_questions(){
 	question1
