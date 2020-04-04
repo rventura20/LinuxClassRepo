b91a889c6b7766827fd717cf5d31c4efd88a632a
Danny Montehermoso <dannymontehermoso56@gmail.com>
2020-04-04 02:55:48 +0000

    submitting hw..almost

--- Lectures/Week09_Python_Curl_And_Rest/Server.py
+++ Lectures/Week09_Python_Curl_And_Rest/Server.py
@@ -32,7 +32,7 @@ class MyFirstServer(BaseHTTPRequestHandler):
 if __name__ == "__main__":
 		addr = "127.0.0.1"
 		#addr = "localhost"
-		port = 80
+		port = 8000
 		server_address = (addr, port)
 		httpd = HTTPServer(server_address, MyFirstServer)
 
