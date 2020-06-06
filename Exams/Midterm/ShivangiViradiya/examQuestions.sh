b375d6600b5a8919d7c5373d106eb62722fc12aa
Shivangi6089 <sviradiya@njcu.edu>
2020-06-06 23:26:34 +0000

    added my exam questions

--- Exams/Midterm/ShivangiViradiya/examQuestions.sh
+++ Exams/Midterm/ShivangiViradiya/examQuestions.sh
@@ -1,13 +1,83 @@
 question1(){
-	echo "todo"
+	echo "What is the function of cp command in linux."
+	echo "a) copying files and directory"
+	echo "b) change the directories"
+	echo "c) delete a file"
+	echo "d) remove a file"
+	read answer
+	case $answer in
+		"a")
+			echo "$RIGHT" >> $ANSWER_FILE
+			;;
+                 "b")
+			 echo "$WRONG" >> $ANSWER_FILE
+			 ;;
+		 "c")
+		         echo "$WRONG" >> $ANSWER_FILE	
+			 ;;
+		 "d") 
+			 echo "$WRONG" >> $ANSWER_FILE
+			 ;;
+		   *) 
+		         echo "Please enter a valid selection from a-d"
+	                 question1
+	                 ;;
+	 esac		 
+		 
 }
 
 question2(){
-	echo "todo"
+	echo "Which command is used for create new directory?"
+	echo "a) pwd"
+	echo "b) mkdir"
+	echo "c) touch"
+	echo "d) mv"
+	read answer
+	case $answer in
+		"a")
+			echo "$WRONG" >> $ANSWER_FILE
+			;;
+                 "b")
+			 echo "$RIGHT" >> $ANSWER_FILE
+			 ;;
+		 "c")
+		         echo "$WRONG" >> $ANSWER_FILE	
+			 ;;
+		 "d") 
+			 echo "$WRONG" >> $ANSWER_FILE
+			 ;;
+		   *) 
+		         echo "Please enter a valid selection from a-d"
+	                 question2
+	                 ;;
+	 esac		
 }
 
 question3(){
-	echo "todo"
+	echo " _______ command is used for removing files in linux."
+	echo "a) ls"
+	echo "b) mv"
+	echo "c) rm"
+	echo "d) cp"
+	read answer
+	case $answer in
+		"a")
+			echo "$WRONG" >> $ANSWER_FILE
+			;;
+                 "b")
+			 echo "$WRONG" >> $ANSWER_FILE
+			 ;;
+		 "c")
+		         echo "$RIGHT" >> $ANSWER_FILE	
+			 ;;
+		 "d") 
+			 echo "$WRONG" >> $ANSWER_FILE
+			 ;;
+		   *) 
+		         echo "Please enter a valid selection from a-d"
+	                 question3
+	                 ;;
+	 esac	
 }
 
 run_shivangis_questions(){
