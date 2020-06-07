410d483bf88f79e803c6e8eaaf25e4d5397ab185
Marcus Sison <marcussison6@gmail.com>
2020-06-07 15:26:35 +0000

    added math.py in my directory

---
+++ Homework_Summer_2020/Week03/Assignment01/MarcusSison/math.py
@@ -0,0 +1,23 @@
+def add(a, b):
+	return a + b
+
+def sub(a, b):
+	return a - b
+
+def mul(a, b):
+	#TODO return the right thing
+	return a * b
+
+def div(a, b):
+	# TODO return the right thing
+	return a / b
+
+if __name__ == "__main__":
+	a = 6
+	b = 3
+	print(add(a,b))
+	print(sub(a,b))
+	print(mul(a,b))
+	print(div(a,b))
+
+
