6c53883616ae12682b398c469a3c2f9aa1340d36
Marcus Sison <marcussison6@gmail.com>
2020-06-07 21:54:41 +0000

    added my exams questions

--- Exams/Midterm/MarcusSison/examQuestions.sh
+++ Exams/Midterm/MarcusSison/examQuestions.sh
@@ -1,13 +1,84 @@
 question1(){
-	echo "todo"
+	echo "What will show you all the subdirectories of the current directory"
+	echo "a) ls"
+	echo "b) cat"
+	echo "c) type"
+	echo "d) biff"
+	read answer
+	case $answer in
+		"a" )
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;; #break
+		"b" )
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"c" )
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"d" ) 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*)
+			echo "Please enter a valid selection from a-d"
+			question1
+			;;
+	esac
+		
 }
 
 question2(){
-	echo "todo"
+	echo "How to list files in reverse order?"
+	echo "a) ls"
+	echo "b) ls -r"
+	echo "c) ls -t"
+	echo "d) ls -ltr"
+	read answer
+	case $answer in
+		"a" ) 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"b" ) 
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;;
+		"c" )	
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"d" )   
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*)
+			echo "Please enter a valid selection from a-d"
+			question2
+			;;
+	esac
 }
 
 question3(){
-	echo "todo"
+	echo "Which command is used to display line of text/string that are passed as an arguement?"
+	echo "a) cp"
+	echo "b) rm"
+	echo "c) echo"
+	echo "d) ls"
+	read answer
+	case $answer in
+		"a" ) 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"b" ) 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"c" ) 
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;;
+		"d" ) 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*) 
+			echo "Please enter a valid selection from a-d"
+			question3
+			;;
+	esac
+
 }
 
 run_marcus_questions(){
