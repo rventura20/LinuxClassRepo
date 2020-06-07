01375127c446f8c46fe1ee6a8b29e10f497d2688
Andrew Kamta <andrewkamta47@hotmail.com>
2020-06-07 20:55:03 +0000

    added my exam questions

--- Exams/Midterm/AndrewKamta/examQuestions.sh
+++ Exams/Midterm/AndrewKamta/examQuestions.sh
@@ -1,13 +1,83 @@
 question1(){
-	echo "todo"
+	echo "Which command do you use to insert a text file to grep"
+	echo "a) wget"
+	echo "b) git get"
+	echo "c) insert file.txt"
+	echo "d) just copy and past link"
+	read answer
+	case $answer in
+		"a")
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;;
+		"b")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"c")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"d") 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*)
+			echo "Please enter a valid selection from a-d"
+			question1
+			;;
+		esac
 }
 
 question2(){
-	echo "todo"
+	echo "Fill in the blank to find all matching words, the, in the text file : grep ____ the pg1.txt | wc -l"
+	echo "a) -i"
+	echo "b) -wi"
+	echo "c) -f"
+	echo "d) -the"
+	read answer
+	case $answer in
+		"a")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"b")
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;;
+		"c") 
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"d")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*)
+			echo "Please enter a valid selction from a-d"
+			question2
+			;;
+		esac
+
 }
 
 question3(){
-	echo "todo"
+	echo "Which command allows you to find the lines without the word the, using grep?"
+	echo "a) grep -i"
+	echo "b) grep -wo"
+	echo "c) grep -v"
+	echo "d) grep -the"
+	read answer
+	case $answer in
+		"a")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"b")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		"c")
+			echo "$RIGHT" >> "$ANSWER_FILE"
+			;;
+		"d")
+			echo "$WRONG" >> "$ANSWER_FILE"
+			;;
+		*)
+			echo "Please enter a valid selection from a-d"
+			question3
+			;;
+		esac
 }
 
 run_andrews_questions(){
